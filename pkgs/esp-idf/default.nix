74c057464432fc5ddaea0fa2b561f85092443e2e
Christoffer Jansson (InCoax) <christoffer.jansson@incoax.com>
2026-07-15 21:29:25 +0200

    Bumped to 6.0.2

--- pkgs/esp-idf/default.nix
+++ pkgs/esp-idf/default.nix
@@ -1,7 +1,7 @@
 {
   owner ? "espressif",
   repo ? "esp-idf",
-  rev ? "v6.0.1",
+  rev ? "v6.0.2",
   sha256 ? "sha256-4KJa686qc+u7XkF/GS2o53l1SpwP2EmdqAn/qmlL1yU=",
   toolsToInclude ? [
     "xtensa-esp-elf-gdb"
